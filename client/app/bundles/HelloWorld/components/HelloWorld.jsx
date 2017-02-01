6ea62a9e62e70f6f4d918c8821939c1486ddab21
Raphael Ferreira <raphaelbsferreira@gmail.com>
2017-02-01 00:56:56 -0200

    initial commit

---
+++ client/app/bundles/HelloWorld/components/HelloWorld.jsx
@@ -0,0 +1,44 @@
+import React, { PropTypes } from 'react';
+import { Textfield } from 'react-mdl';
+
+export default class HelloWorld extends React.Component {
+  static propTypes = {
+    name: PropTypes.string.isRequired, // this is passed from the Rails view
+  };
+
+  /**
+   * @param props - Comes from your rails view.
+   * @param _railsContext - Comes from React on Rails
+   */
+  constructor(props, _railsContext) {
+    super(props);
+
+    // How to set initial state in ES6 class syntax
+    // https://facebook.github.io/react/docs/reusable-components.html#es6-classes
+    this.state = { name: '' };
+  }
+
+  updateName = (name) => {
+    this.setState({ name });
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>
+          Hello, {this.state.name}!
+        </h3>
+        <hr />
+        <form >
+          <Textfield
+            id="name"
+            type="text"
+            label="Say hello to:"
+            value={this.state.name}
+            onChange={(e) => this.updateName(e.target.value)}
+          />
+        </form>
+      </div>
+    );
+  }
+}
